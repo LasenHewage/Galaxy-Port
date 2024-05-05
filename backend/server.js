833892087b94f42f663b40fa4e5fd04619a8942a
LasenHewage <lasenhewage111@gnail.com>
2024-05-05 17:00:56 +0530

    change host url

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const app = express();
 app.use(express.json());
 
 // CORS middleware
-app.use(cors({ origin: 'https://steady-dieffenbachia-a3dff0.netlify.app' }));
+app.use(cors({ origin: 'https://galaxy-port.netlify.app' }));
 
 // Logger middleware
 app.use((req, res, next) => {
